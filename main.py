e9bd90bc5c8cfc33fc9c8429193ff00e3a788e90
GabrielSpc1 <gabriel_ml@sportcarautopecas.com.br>
2025-07-15 16:13:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from extrair_metadados_meli import executar_extracao
+from extrair_metadados_meli import executar_extracao_por_status
 from extrair_perguntas_respondidas import executar_extracao_perguntas
 
 app = FastAPI()
@@ -8,11 +8,27 @@ app = FastAPI()
 def raiz():
     return {"mensagem": "API de extração Mercado Livre ativa."}
 
-@app.get("/extrair-metadados")
-def extrair_metadados():
+@app.get("/extrair-metadados-ativos")
+def extrair_ativos():
     try:
-        executar_extracao()
-        return {"status": "sucesso", "mensagem": "Metadados extraídos e enviados para o GitHub"}
+        executar_extracao_por_status("active", "produtos_meli_ativos.jsonl", "produtos_meli_ativos.jsonl")
+        return {"status": "sucesso", "mensagem": "Ativos extraídos e enviados para o GitHub"}
+    except Exception as e:
+        return {"status": "erro", "mensagem": str(e)}
+
+@app.get("/extrair-metadados-pausados")
+def extrair_pausados():
+    try:
+        executar_extracao_por_status("paused", "produtos_meli_pausados.jsonl", "produtos_meli_pausados.jsonl")
+        return {"status": "sucesso", "mensagem": "Pausados extraídos e enviados para o GitHub"}
+    except Exception as e:
+        return {"status": "erro", "mensagem": str(e)}
+
+@app.get("/extrair-metadados-finalizados")
+def extrair_finalizados():
+    try:
+        executar_extracao_por_status("closed", "produtos_meli_finalizados.jsonl", "produtos_meli_finalizados.jsonl")
+        return {"status": "sucesso", "mensagem": "Finalizados extraídos e enviados para o GitHub"}
     except Exception as e:
         return {"status": "erro", "mensagem": str(e)}
 
