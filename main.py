6c220e119272d109b2ef8a8cfff08e5f1abdd690
GabrielSpc1 <gabriel_ml@sportcarautopecas.com.br>
2025-07-17 09:49:45 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+
+from fastapi import FastAPI, Query
 from extrair_ativos import executar_extracao_ativos
 from extrair_pausados import executar_extracao_pausados
 from extrair_finalizados import executar_extracao_finalizados
@@ -11,26 +12,26 @@ def raiz():
     return {"mensagem": "API de extração Mercado Livre ativa."}
 
 @app.get("/extrair-ativos")
-def ativos():
+def ativos(offset: int = Query(0), limit: int = Query(1000)):
     try:
-        executar_extracao_ativos()
-        return {"status": "sucesso", "mensagem": "Ativos extraídos com sucesso"}
+        executar_extracao_ativos(offset=offset, limit=limit)
+        return {"status": "sucesso", "mensagem": f"Ativos extraídos com offset={offset} e limit={limit}"}
     except Exception as e:
         return {"status": "erro", "mensagem": str(e)}
 
 @app.get("/extrair-pausados")
-def pausados():
+def pausados(offset: int = Query(0), limit: int = Query(1000)):
     try:
-        executar_extracao_pausados()
-        return {"status": "sucesso", "mensagem": "Pausados extraídos com sucesso"}
+        executar_extracao_pausados(offset=offset, limit=limit)
+        return {"status": "sucesso", "mensagem": f"Pausados extraídos com offset={offset} e limit={limit}"}
     except Exception as e:
         return {"status": "erro", "mensagem": str(e)}
 
 @app.get("/extrair-finalizados")
-def finalizados():
+def finalizados(offset: int = Query(0), limit: int = Query(1000)):
     try:
-        executar_extracao_finalizados()
-        return {"status": "sucesso", "mensagem": "Finalizados extraídos com sucesso"}
+        executar_extracao_finalizados(offset=offset, limit=limit)
+        return {"status": "sucesso", "mensagem": f"Finalizados extraídos com offset={offset} e limit={limit}"}
     except Exception as e:
         return {"status": "erro", "mensagem": str(e)}
 
